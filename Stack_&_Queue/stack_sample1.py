09624eee224bc1567fb798b270553ccaf288136f
Merin Mathew <merinphilaminmathew19@gmail.com>
2024-04-08 12:03:22 +0530

    string reverse by stack

--- Stack_&_Queue/stack_sample1.py
+++ Stack_&_Queue/stack_sample1.py
@@ -26,7 +26,22 @@ class Stack:
         while temp is not None:
             print(temp.data)
             temp = temp.next
-        
+
+    def reverse_str(self, string):
+        # Create an empty stack
+        stack = Stack()
+
+        # Push each character of the string onto the stack
+        for char in string:
+            stack.push(char)
+
+        # Build the reversed string by popping characters from the stack
+        reversed_string = ""
+        while stack.top is not None:
+            reversed_string += stack.top.data
+            stack.pop()
+
+        return reversed_string
 
 list = Stack()
 list.push(10)
@@ -37,3 +52,6 @@ list.push(50)
 
 #list.pop()
 list.display()
+str = "aalrib atat"
+reversed = list.reverse_str(str)
+print(reversed)
