46e3a59c66c634a32eb5be3432b169399f3d72c3
Merin Mathew <merinphilaminmathew19@gmail.com>
2024-04-16 23:21:01 +0530

    week3__

--- Basic Operations/Sorting/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-if left_arr[i]<right_arr[j]:
-                arr[k]=left_arr[i]
-                i+=1
-            else:
-                arr[k]=right_arr[j]
-                j+=1
-            k+=1
